ab84fe4df7f28032facd4c0e7b9c1a3a37fb5c42
catninpo <github@cat.ninja>
2024-03-15 14:53:01 +0000

    Add the ability to create a thumbnail and search image tags
    Implements functionality to scale down uploaded images to a secondary thumbnail of 100x100 size, as well as implements very basic
    server based rendered searching.

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,11 @@ use axum::{
     http::{header, HeaderMap},
     response::{Html, IntoResponse},
     routing::{get, post},
-    Extension, Router,
+    Extension, Form, Json, Router,
 };
-use sqlx::Row;
+use futures::TryStreamExt;
+use serde::{Deserialize, Serialize};
+use sqlx::{FromRow, Pool, Row, Sqlite};
 use tokio_util::io::ReaderStream;
 
 #[tokio::main]
@@ -16,7 +18,10 @@ async fn main() -> anyhow::Result<()> {
         .route("/", get(home_page))
         .route("/upload", post(uploader))
         .route("/image/:id", get(get_image))
+        .route("/thumb/:id", get(get_thumbnail))
+        .route("/images", get(list_images))
         .route("/image-count", get(image_count_page))
+        .route("/search", post(search_images))
         .layer(Extension(pool));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -32,6 +37,8 @@ async fn setup() -> anyhow::Result<sqlx::SqlitePool, anyhow::Error> {
     let db_pool = sqlx::SqlitePool::connect(&db_url).await?;
 
     sqlx::migrate!("./migrations").run(&db_pool).await?;
+
+    fill_missing_thumbnails(&db_pool).await?;
 
     Ok(db_pool)
 }
@@ -106,10 +113,39 @@ async fn get_image(Path(id): Path<i64>) -> impl IntoResponse {
         .unwrap()
 }
 
+// TODO: Make generic with get_image
+async fn get_thumbnail(Path(id): Path<i64>) -> impl IntoResponse {
+    let filename = format!("images/{id}_thumb.jpg");
+    let attachment = format!("filename={filename}");
+    let mut headers = HeaderMap::new();
+    headers.insert(
+        header::CONTENT_TYPE,
+        header::HeaderValue::from_static("image/jpeg"),
+    );
+    headers.insert(
+        header::CONTENT_DISPOSITION,
+        header::HeaderValue::from_str(&attachment).unwrap(),
+    );
+
+    let file = tokio::fs::File::open(&filename).await.unwrap();
+
+    axum::response::Response::builder()
+        .header(
+            header::CONTENT_TYPE,
+            header::HeaderValue::from_static("image/jpeg"),
+        )
+        .header(
+            header::CONTENT_DISPOSITION,
+            header::HeaderValue::from_str(&attachment).unwrap(),
+        )
+        .body(axum::body::Body::from_stream(ReaderStream::new(file)))
+        .unwrap()
+}
+
 async fn uploader(
     Extension(pool): Extension<sqlx::SqlitePool>,
     mut multipart: Multipart,
-) -> String {
+) -> Html<String> {
     let mut tags = None;
     let mut image = None;
 
@@ -128,9 +164,94 @@ async fn uploader(
         // TODO: Return response header instead on failure rather than erroring out.
         let image_id = store_image_to_database(&pool, &tags).await.unwrap();
         save_image(image_id, &image).await.unwrap();
+        tokio::task::spawn_blocking(move || {
+            make_thumbnail(image_id).unwrap(); // TODO Handle Error.
+        });
     } else {
         panic!("Missing field"); // TODO: Handle Error. -> Return 400 Bad Request
     }
 
-    "Ok".to_string()
+    let path = std::path::Path::new("src/pages/redirect.html");
+    let content = tokio::fs::read_to_string(path).await.unwrap();
+
+    Html(content)
+}
+
+async fn fill_missing_thumbnails(pool: &Pool<Sqlite>) -> anyhow::Result<()> {
+    let mut rows = sqlx::query("SELECT id FROM images").fetch(pool);
+
+    while let Some(row) = rows.try_next().await? {
+        let id = row.get::<i64, _>(0);
+        let thumbnail_path = format!("images/{id}_thumb.jpg");
+        if !std::path::Path::new(&thumbnail_path).exists() {
+            tokio::task::spawn_blocking(move || make_thumbnail(id)).await??;
+        }
+    }
+
+    Ok(())
+}
+
+fn make_thumbnail(id: i64) -> anyhow::Result<()> {
+    let image_path = format!("images/{id}.jpg");
+    let thumbnail_path = format!("images/{id}_thumb.jpg");
+    let image_bytes: Vec<u8> = std::fs::read(image_path)?;
+
+    let image = if let Ok(format) = image::guess_format(&image_bytes) {
+        image::load_from_memory_with_format(&image_bytes, format)?
+    } else {
+        image::load_from_memory(&image_bytes)?
+    };
+
+    let thumbnail = image.thumbnail(100, 100);
+    thumbnail.save(thumbnail_path)?;
+
+    Ok(())
+}
+
+#[derive(Deserialize, Serialize, FromRow, Debug)]
+struct ImageRecord {
+    id: i64,
+    tags: String,
+}
+
+async fn list_images(Extension(pool): Extension<sqlx::SqlitePool>) -> Json<Vec<ImageRecord>> {
+    sqlx::query_as::<_, ImageRecord>("SELECT id, tags FROM images ORDER BY id")
+        .fetch_all(&pool)
+        .await
+        .unwrap()
+        .into()
+}
+
+#[derive(Deserialize)]
+struct Search {
+    tags: String,
+}
+
+async fn search_images(
+    Extension(pool): Extension<sqlx::SqlitePool>,
+    Form(form): Form<Search>,
+) -> Html<String> {
+    let tag = format!("%{}%", form.tags);
+
+    let rows = sqlx::query_as::<_, ImageRecord>(
+        "SELECT id, tags FROM images WHERE tags LIKE ? ORDER BY id",
+    )
+    .bind(tag)
+    .fetch_all(&pool)
+    .await
+    .unwrap();
+
+    let mut results = String::new();
+    for row in rows {
+        results.push_str(&format!(
+            "<a href='image/{}'><img src='/thumb/{}'/></a><br/>",
+            row.id, row.id
+        ));
+    }
+
+    let path = std::path::Path::new("src/pages/search.html");
+    let mut content = tokio::fs::read_to_string(path).await.unwrap();
+    content = content.replace("{results}", &results);
+
+    Html(content)
 }
